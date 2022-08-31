e6d972ead5fd5b8f59801948e2036c35fe94edb0
agungnursatria <agungnursatria@gmail.com>
2022-08-31 21:02:46 +0700

    Create home service

---
+++ lib/screens/home/service/service.dart
@@ -0,0 +1,27 @@
+import 'package:cloudy/config/network/error/network_error.dart';
+import 'package:cloudy/config/network/http/network_http_interface.dart';
+import 'package:cloudy/config/network/http/network_http_model.dart';
+import 'package:dartz/dartz.dart';
+
+/// [HomeService] has responsibility to fetch the data from the API
+///
+class HomeService {
+  NetworkHttpInterface network;
+  HomeService({required this.network});
+
+  /// This function to fetch weather forcast from API
+  Future<Either<NetworkModel, NetworkError>> getWeatherForecast({
+    required String lat,
+    required String lon,
+    required String appId,
+  }) async {
+    return await network.requestGetNoAuth(
+      path: '/forceast',
+      queryParameters: {
+        'lat': lat,
+        'lon': lon,
+        'appId': appId,
+      },
+    );
+  }
+}
