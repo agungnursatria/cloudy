ad5965ee2363c1d8172e205f1bdf697d248926fd
agungnursatria <agungnursatria@gmail.com>
2022-08-31 21:02:31 +0700

    Add api key to env file

--- lib/config/environment/env.dart
+++ lib/config/environment/env.dart
@@ -4,8 +4,10 @@ class Env {
   factory Env() => _instance;
 
   late String baseUrl;
+  late String apiKey;
 
   void initEnv() {
-    baseUrl = "https://openweathermap.org/";
+    baseUrl = "https://api.openweathermap.org/data/2.5";
+    apiKey = "1fda9a057f0e2e9f4faa7dad5213e153";
   }
 }
